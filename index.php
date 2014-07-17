51aef62f357d3f934952650f0a6491d1dc16bd55
root <root@localhost.localdomain>
2014-07-17 19:42:51 +0100

    clean II

--- index.php
+++ index.php
@@ -41,7 +41,7 @@
       <div class="page-header">
 	<pre>
 	 </pre>
-        <h1>Patagonia-Images dev branch</h1>
+        <h1>Patagonia-Images dev. branch</h1>
         <p class="lead">Basic grid layouts to get you familiar with building within the Bootstrap grid system.</p>
       </div>
 
