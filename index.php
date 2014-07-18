9dd9242f81e0ea45e73ac2d14472e5dfc362db00
root <root@localhost.localdomain>
2014-07-18 16:36:22 +0100

    text edit

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 
     <link rel="icon" href="http://getbootstrap.com/favicon.ico">
 
-    <title>Patagonia Images - dev branch</title>
+    <title>Patagonia Images |   'page'   </title>
 
     <!-- Bootstrap core CSS -->
     <link href="grid-template_files/bootstrap.css" rel="stylesheet">
