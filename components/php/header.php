bf2f8c9c74b21ae1437c31e9280914e437231e0a
root <root@localhost.localdomain>
2014-07-18 16:20:11 +0100

    modularize header. main-content. ...

---
+++ components/php/header.php
@@ -0,0 +1,6 @@
+      <div class="page-header">
+	<pre>
+	 </pre>
+        <h1>Patagonia-Images dev. branch</h1>
+        <p class="lead">Basic grid layouts to get you familiar with building within the Bootstrap grid system.</p>
+      </div>
