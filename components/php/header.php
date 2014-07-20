fb22c3a9b8906c489e999f9a10d1a2d93e0fc530
jordila <jla@riseup.net>
2014-07-20 07:57:12 +0100

    +-

--- components/php/header.php
+++ components/php/header.php
@@ -1,6 +1,4 @@
       <div class="page-header">
-	<pre>
-	 </pre>
         <h1>Patagonia-Images dev. branch</h1>
         <p class="lead">Basic grid layouts to get you familiar with building within the Bootstrap grid system.</p>
       </div>
