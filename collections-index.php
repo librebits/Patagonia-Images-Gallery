5770456622e76681ff33300426287a4eba78179b
jordila <jla@riseup.net>
2014-08-31 09:11:51 -0400

    adaptado css-image-gallery with dynamic caption text

--- collections-index.php
+++ collections-index.php
@@ -63,101 +63,69 @@
 <!-- ++ as per Bootstrap-Images-Gallery -->
     <link rel="stylesheet" href="css/bootstrap-image-gallery.min.css">
     <link rel="stylesheet" href="css/style.css">
-    
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
 
   </head>
 
   <body id="home">
 
 	<!-- { navbar-->
-	<?php include "components/php/navbar.php"; ?>
-	    <section class="container">
+    <?php include "components/php/navbar.php"; ?>
+      
+      <section class="container">
 
- <!--++  here goes links: (fruits, vegetables) gallery. -->
+        <p> Blab,bla..................................................................bla. Ble. </p>
+	
+	<div id="wrapper">
+	  <div id="gallery" class="clearfix">
+	    <figure>
 
-  <div id="links">
+              <img src="img/collections-index/AAT-001_Patagonia_-Amanecer-en-la-Torre-Principal_cerro-Catedral.jpg" alt="Amanecer-en-la-Torre-Principal_cerro-Catedral">
+              <figcaption>
+		AAT(<a href="file://collections-AAT.php" target="_blank">Source</a>)
+              </figcaption>
+	    </figure>
+	    
+	    <figure>
+              <img src="img/collections-index/AMR-001_Patagonia_Gaviotas-de-Olrog_Chubut.jpg" alt="Patagonia_Gaviotas-de-Olrog_Chubut">
+              <figcaption class="light">
+		AMR(<a href="collections-AMR.php" target="_blank">Source</a>)
+              </figcaption>
+	    </figure>
+	    
+	    <figure>
+              <img src="img/collections-index/AYO-004_Patagonia_-Cosechando-frambuesas_Cabaña-Mico_El-Bolson.jpg" alt="Patagonia_-Cosechando-frambuesas_Cabaña-Mico_El-Bolson">
+              <figcaption>
+		AYO (<a href="file://collections-AYO.php" target="_blank">Source</a>)
+              </figcaption>
+	    </figure>
+	    
+	    <figure>
+              <img src="img/collections-index/BAM-002_Patagonia_Cola-de-la-ballena-Franca-Austral_-Peninsula-Valdes.jpg" alt="Cola-de-la-ballena-Franca-Austral_-Peninsula-Valdes">
+              <figcaption>
+		BAM (<a href="file://collections-BAM.php" target="_blank">Source</a>)
+              </figcaption>
+	    </figure>
+	    
+	    <figure>
+              <img src="img/collections-index/BAN-001_Patagonia_Bosque-de-Coihues-en-la-niebla_Selva-Valdiviana.jpg" alt="Bosque-de-Coihues-en-la-niebla_Selva-Valdiviana">
+              <figcaption>
+		BAN (<a href="file://collections-BAN.php" target="_blank">Source</a>)
+              </figcaption>
+	    </figure>
+	    
+	    <figure>
+              <img src="img/collections-index/CAM-011_Patagonia_Camino-a-casa_Chubut.jpg" alt="Patagonia_Camino-a-casa_Chubut">
+              <figcaption>
+		CAM (<a href="file://collections-CAM.php" target="_blank">Source</a>)
+              </figcaption>
+	    </figure>
+	  </div> <!-- end img gallery wrapper -->
 
-    <a href="http://placehold.it/500x333/333" title="Amaneceres y Atardeceres" >
-	<img src="http://placehold.it/250x167/333" alt="Amaneceres y Atardeceres">
-    </a>
 
-    <a href="http://placehold.it/500x333" title="Artesanos y Oficios" >
-	<img src="http://placehold.it/250x167" alt="Artesanos y Oficios">
-    </a>
+      </section>
 
-    <a href="http://placehold.it/500x333/aaa" title="Costa" >
-	<img src="http://placehold.it/250x167/aaa" alt="Costa">
-    </a>
-
-    <a  href="http://placehold.it/500x333/ccc" title="Estepa" >
-	<img src="http://placehold.it/250x167/ccc" alt="Estepa">
-    </a>
-    
-    <a href="http://placehold.it/500x333/333" title="Banana" >
-	<img src="http://placehold.it/250x167/333" alt="BAnana">
-    </a>
-
-    <a href="http://placehold.it/500x333" title="Melon" >
-	<img src="http://placehold.it/250x167" alt="Melon">
-    </a>
-
-    <a  href="http://placehold.it/500x333/eee" title="Maracuya" >
-	<img src="http://placehold.it/250x167/eee" alt="Maracuya">
-    </a>
-
-    <a href="http://placehold.it/500x333/fff" title="Banana" >
-	<img src="http://placehold.it/250x167/fff" alt="BAnana">
-    </a>
-
-    <a href="http://placehold.it/500x333/aaa" title="Melon" >
-	<img src="http://placehold.it/250x167/aaa" alt="Melon">
-    </a>
-
-    <a  href="http://placehold.it/500x333/ccc" title="Maracuya" >
-	<img src="http://placehold.it/250x167/ccc" alt="Maracuya">
-    </a>
-
-
-    <a href="http://placehold.it/500x333/333" title="Banana" >
-	<img src="http://placehold.it/250x167/333" alt="BAnana">
-    </a>
-
-    <a href="http://placehold.it/500x333" title="Melon" >
-	<img src="http://placehold.it/250x167" alt="Melon">
-    </a>
-
-    <a  href="http://placehold.it/500x333/eee" title="Maracuya" >
-	<img src="http://placehold.it/250x167/eee" alt="Maracuya">
-    </a>
-
-    <a href="http://placehold.it/500x333/fff" title="Lemmon" >
-	<img src="http://placehold.it/250x167/fff" alt="Lemmon">
-    </a>
-
-    <a href="http://placehold.it/500x333/aaa" title="Melon" >
-	<img src="http://placehold.it/250x167/aaa" alt="Melon">
-    </a>
-
-    <a  href="http://placehold.it/500x333/ccc" title="Maracuya" >
-	<img src="http://placehold.it/250x167/ccc" alt="Maracuya">
-    </a>
-
-    <a href="http://placehold.it/500x333/333" title="Orange" >
-	<img src="http://placehold.it/250x167/333" alt="Orange">
-    </a>
-
-    <a href="http://placehold.it/500x333" title="Melon" >
-	<img src="http://placehold.it/250x167" alt="Melon">
-    </a>
-
-    <a  href="http://placehold.it/500x333/eee" title="Maracuya" >
-	<img src="http://placehold.it/250x167/eee" alt="Maracuya">
-    </a>
-
-    <a href="http://placehold.it/500x333/fff" title="Pera" >
-	<img src="http://placehold.it/250x167/fff" alt="Pera">
-    </a>
-  </div>
+            
 
 
  	     <!-- here goes main-content.php comp. -->
